9233e8299b915df14ce572f3aca40c73114216f3
T-Carri <bryan_narutito@hotmail.com>
2020-07-15 20:55:25 -0500

    ok

---
+++ src/app/route.js
@@ -0,0 +1,53 @@
+module.exports=(app, passport)=> {
+
+   app.get('/', (req, res)=>{ res.render('index');
+});
+
+
+
+// login 
+app.get('/login', (req, res)=>{
+   res.render('login.ejs', {
+      message: req.flash('loginMessage')
+   });
+});
+
+app.post('/login', passport.authenticate('local-login',{
+   successRedirect:'/T1',
+   failureRedirect:'/login',
+   failureFlash: true
+}));
+
+//signup
+app.get('/signup', (req, res)=>{
+   res.render('signup.ejs', {
+      message: req.flash('signupMessage')
+   });
+});
+app.post('/signup', passport.authenticate('local-signup',{
+   successRedirect:'/login',
+   failureRedirect:'/signup',
+   failureFlash: true //allow flash messages
+}));
+
+//T1
+app.get('/T1', isLoggedIn, (req, res) => { 
+   res.render('T1', {
+      user: req.user
+ });
+});
+
+
+//logout
+app.get('/logout',(req, res)=>{
+   req.logout();
+   res.redirect('/');
+});
+};
+
+function isLoggedIn(req, res, next){
+   if(req.isAuthenticated()){
+      return next();
+   }
+   res.redirect('/');
+}
---
+++ src/config/passport.js
@@ -0,0 +1,65 @@
+const LocalStrategy= require('passport-local').Strategy;
+const User=require('../app/models/user');
+
+module.exports=function(passport){
+//required for persistent loggin sessions
+//passport needs ability to serialize and unserialize an userialize users out of session
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+//used to deserialize user
+passport.deserializeUser(function(id,done){
+    User.findById(id, function(err,user){
+        done(err, user);
+    });
+});
+
+//singup
+passport.use('local-signup', new LocalStrategy({
+    //by default local strategy uses username and password, we will override with email
+  //  userfield: 'user',
+  usernameField:'email',
+  passwordField:'password',
+ passReqToCallback:true //allow us to pass back the entire request  to the callback    
+}, function(req, email, password, done){
+    console.log(" email es:" + email, "password es:"+ password);
+    User.findOne({'local.email':email}, function(err, user){
+        if(err){
+            return done(err);
+        }
+        if(user){
+            return done(null, false, req.flash('signupMessage', 'Este correo ya esta registrado'));
+        } else { 
+            var newUser= new User();
+          //  newUser.local.user=user;
+            newUser.local.email=email;
+            newUser.local.password=newUser.generateHash(password);
+            newUser.save(function(err){
+                if(err){throw err;}
+            return done(null, newUser);
+    });
+        }
+    });
+}));
+
+//login
+//we are using named strategies since we have one for login and one for singup
+//by default, if there was no name, it would just be called 'local'
+passport.use('local-login', new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+ passReqToCallback:true
+}, function(req, email, password, done){
+    console.log("el email es:" + email, "password es:"+ password);
+    User.findOne({'local.email':email}, function(err, user){
+        if(err){return done(err);
+        }
+        if(!user){
+            return done(null, false, req.flash('loginMessage', 'Usuario incorrecto'));
+        } if(!user.validPassword(password)){
+            return done(null, false, req.flash('loginMessage', 'Contraseña incorrecta'));
+        }
+        return done (null, user);
+    });
+}));
+}
